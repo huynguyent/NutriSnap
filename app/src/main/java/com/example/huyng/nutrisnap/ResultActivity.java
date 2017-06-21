ad7c41187597bfd89654680a23e2988c546bd236
Huy Nguyen <huy.ngtuan@gmail.com>
2017-06-21 23:08:17 +0700

    Add a basic ResultActivity, Allow main buttons to change color when focused

---
+++ app/src/main/java/com/example/huyng/nutrisnap/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.example.huyng.nutrisnap;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
